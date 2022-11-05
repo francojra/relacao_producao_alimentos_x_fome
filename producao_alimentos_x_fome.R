acf8c4f75e36e14e258061e3897e6f53c99552e5
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-05 20:20:33 -0300

    Add files via upload

--- producao_alimentos_x_fome.R
+++ producao_alimentos_x_fome.R
@@ -7,7 +7,7 @@
 # Países incluídos nas análises ------------------------------------------------------------------------------------------------------------
 
 ### India, China, Estados Unidos, Brasil, Russia, México,
-### Alemanha, Espanha, Argentina e Canada.
+### Espanha, Argentina e Canada.
 
 # Período ----------------------------------------------------------------------------------------------------------------------------------
 
@@ -22,15 +22,58 @@ library(cols4all)
 
 carne <- read.csv("meat-production-tonnes.csv")
 view(carne)
+names(carne)
 trigo <- read.csv("wheat-production.csv")
 view(trigo)
+names(trigo)
 arroz <- read.csv("rice-production.csv")
 view(arroz)
+names(arroz)
 batata <- read.csv("potato-production.csv")
 view(batata)
+names(batata)
 fome <- read.csv("prevalence-of-undernourishment.csv")
 view(fome)
+names(fome)
 
 # Manipular dados --------------------------------------------------------------------------------------------------------------------------
 
+carne <- carne %>%
+  select(-Code) %>%
+  filter(Entity %in% c("India", "China", "Brazil", "United States", 
+                       "Mexico", "Spain", "Russia", "Canada", "Argentina"),
+         between(Year, 2001, 2019)) %>%
+  rename(prod_carne = Meat..total...00001765....Production...005510....tonnes) %>%
+  view()
+  
+trigo <- trigo %>%
+  select(-Code) %>%
+  filter(Entity %in% c("India", "China", "Brazil", "United States", 
+                       "Mexico", "Spain", "Russia", "Canada", "Argentina"),
+         between(Year, 2001, 2019)) %>%
+  rename(prod_trigo = Wheat...00000015....Production...005510....tonnes) %>%
+  view()
 
+arroz <- arroz %>%
+  select(-Code) %>%
+  filter(Entity %in% c("India", "China", "Brazil", "United States", 
+                       "Mexico", "Spain", "Russia", "Canada", "Argentina"),
+         between(Year, 2001, 2019)) %>%
+  rename(prod_arroz = Rice...00000027....Production...005510....tonnes) %>%
+  view()
+
+batata <- batata %>%
+  select(-Code) %>%
+  filter(Entity %in% c("India", "China", "Brazil", "United States", 
+                       "Mexico", "Spain", "Russia", "Canada", "Argentina"),
+         between(Year, 2001, 2019)) %>%
+  rename(prod_batata = Potatoes...00000116....Production...005510....tonnes) %>%
+  view()
+
+fome <- fome %>%
+  select(-Code) %>%
+  filter(Entity %in% c("India", "China", "Brazil", "United States", 
+                       "Mexico", "Spain", "Russia", "Canada", "Argentina"),
+         between(Year, 2001, 2019)) %>%
+  rename(porc_subnut = Prevalence.of.undernourishment....of.population.) %>%
+  view()
